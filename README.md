f7b0f5fc4853deeb3f9e6f828333a719c88e5ce7
Theodor Moroianu <theodor.moroianu@gmail.com>
2024-02-16 17:28:27 +0100

    Update build from the main kactl repo

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# KACTL
+# ICPC Docs
+
+This repo is a fork of KACTL. Below is the README of KACTL.
+
+## KACTL
 
 This repo hosts KACTL, [KTH](https://en.wikipedia.org/wiki/KTH_Royal_Institute_of_Technology)'s ICPC team reference document.
 It consists of 25 pages of copy-pasteable C++ code, for use in ICPC-style programming competitions.
--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -1,17 +1,12 @@
 #include <bits/stdc++.h>
-
 using namespace std;
-using ll = long long;
-const ll INF = 2e18;
 
-ll Ceil(ll a, ll b) {
-	assert(b > 0);
-	return a / b + (a % b > 0);
-}
-ll Floor(ll a, ll b) {
-	assert(b > 0);
-	return a / b - (a % b < 0);
-}
+#define rep(i, a, b) for(int i = a; i < (b); ++i)
+#define all(x) begin(x), end(x)
+#define sz(x) (int)(x).size()
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<int> vi;
 
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
