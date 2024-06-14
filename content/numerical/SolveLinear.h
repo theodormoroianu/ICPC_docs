bee66e6c9939aa4bec40359109e877407cadaf99
Paula Vidas <paulavidas@gmail.com>
2024-06-14 09:59:12 +0200

    fix SolveLinear

--- content/numerical/SolveLinear.h
+++ content/numerical/SolveLinear.h
@@ -14,7 +14,7 @@
 vector<ld> SolveLinear(mat& A) {
   int m = A[0].size() - 1;
   auto piv = RowEchelon(A).first;
-  if (piv.size() > m) return {};
+  if (!piv.empty() && piv.back() == m) return {};
   vector<ld> sol(m, 0.);
   for (int i = 0; i < (int)piv.size(); ++i)
     sol[piv[i]] = A[i][m] / A[i][i];
